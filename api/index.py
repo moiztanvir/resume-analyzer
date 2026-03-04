8e3e6519f8eadf6d694b32c47b174bd2fcc5cede
Moiz Tanvir <151388609+moiztanvir@users.noreply.github.com>
2026-03-04 22:52:31 +0500

    Fix import path for FastAPI application
    Adjust import path for backend.main module.

--- api/index.py
+++ api/index.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from mangum import Mangum
-from backend.main import app as fastapi_app
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from backend.main import app
+# from backend.main import app as fastapi_app
 
 app = fastapi_app
 handler = Mangum(app)
